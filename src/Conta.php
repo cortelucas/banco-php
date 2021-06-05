5c79907b6c2dca98a1624696050ae375ff49e35e
Lucas Corte <corte1994.lc@gmail.com>
2021-06-05 18:41:28 -0300

    implementacao getters e setters

--- src/Conta.php
+++ src/Conta.php
@@ -20,6 +20,54 @@ class Conta
         $this->saldo = $saldo;
     }
 
+    /**
+     * @return string
+     */
+    public function getCpfTitular(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    /**
+     * @param string $cpfTitular
+     */
+    public function setCpfTitular(string $cpfTitular): void
+    {
+        $this->cpfTitular = $cpfTitular;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    /**
+     * @param string $nomeTitular
+     */
+    public function setNomeTitular(string $nomeTitular): void
+    {
+        $this->nomeTitular = $nomeTitular;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    /**
+     * @param float $saldo
+     */
+    public function setSaldo(float $saldo): void
+    {
+        $this->saldo = $saldo;
+    }
+
 
     public function sacar(float $valorSacar): void
     {
